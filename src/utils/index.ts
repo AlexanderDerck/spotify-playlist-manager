2da9a0dccc85edd0c8a377c61482df0544b606cd
Alexander Derck <aderck@gmail.com>
2020-04-09 01:56:33 +0200

    Add StringMap utility type

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,5 @@
 export * from './action-types';
+export * from './create-error-action';
 export * from './props';
+export * from './string-map';
+export * from './with-access-token';
---
+++ src/utils/string-map.ts
@@ -0,0 +1,1 @@
+export type StringMap<TValue> = { [key: string]: TValue };
